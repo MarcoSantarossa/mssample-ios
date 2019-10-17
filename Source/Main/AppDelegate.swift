748afaaf6b4e4d959d864dfdeb6fd1e66e10a35a
Marco Santarossa <marco.santarossa@tide.co>
2019-10-17 14:49:53 +0100

    Remove main storyboard and default view controller

--- Source/Main/AppDelegate.swift
+++ Source/Main/AppDelegate.swift
@@ -6,7 +6,11 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+
         return true
     }
 
