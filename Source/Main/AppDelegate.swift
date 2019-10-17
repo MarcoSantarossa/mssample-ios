339b2ba2d9014f3d12153a559f47e4ca51785ae7
Marco Santarossa <MarcoSantarossa@users.noreply.github.com>
2019-10-17 14:55:57 +0100

    Use mock app delegate for tests (#4)
    * Remove main storyboard and default view controller
    
    * Add main swift file as entry point of the app

--- Source/Main/AppDelegate.swift
+++ Source/Main/AppDelegate.swift
@@ -1,12 +1,15 @@
 import UIKit
 
-@UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+
         return true
     }
 
