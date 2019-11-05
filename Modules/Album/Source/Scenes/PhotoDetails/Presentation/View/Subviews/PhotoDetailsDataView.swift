7a73cad3ccc6062c132f218a3c9e966d90449af8
Marco Santarossa <marcosantadev@gmail.com>
2019-11-05 14:21:54 +0000

    Add loading indicator instead of image placeholder

--- Modules/Album/Source/Scenes/PhotoDetails/Presentation/View/Subviews/PhotoDetailsDataView.swift
+++ Modules/Album/Source/Scenes/PhotoDetails/Presentation/View/Subviews/PhotoDetailsDataView.swift
@@ -4,12 +4,18 @@ final class PhotoDetailsDataView: UIView {
 
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var loadingIndicator: UIActivityIndicatorView!
 
     func configure(title: String) {
         titleLabel.text = title
     }
 
     func update(image: UIImage) {
+        loadingIndicator.removeFromSuperview()
+
         imageView.image = image
+    }
+    deinit {
+        print("deinit \(self)")
     }
 }
